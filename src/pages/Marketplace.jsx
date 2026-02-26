5b47ac6c50d84022827a19ac47874523fb764bdb
samimbey <samimbey@gmail.com>
2026-02-26 03:31:15 +0000

    File changes

--- src/pages/Marketplace.jsx
+++ src/pages/Marketplace.jsx
@@ -17,7 +17,7 @@ const financeOptions = [
   {
     id: "carecredit",
     name: "CareCredit",
-    logo: "https://www.carecredit.com/assets/img/carecredit-logo.svg",
+    logo: "https://www.carecredit.com/sites/cc/image/cc_main_logo_tab.png",
     description: "The leading healthcare credit card for medical expenses, accepted at over 250,000 providers nationwide.",
     aprRange: "0% - 26.99%",
     minCredit: "good",
