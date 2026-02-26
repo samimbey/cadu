20dae6546a08e353ed2ce53b93a0b228977a6161
samimbey <samimbey@gmail.com>
2026-02-26 04:57:53 +0000

    File changes

--- src/pages/Onboarding.jsx
+++ src/pages/Onboarding.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { base44 } from "@/api/base44Client";
 import { createPageUrl } from "@/utils";
 import { AnimatePresence, motion } from "framer-motion";
