507b0d88991c812e42730bdd80eafaaaf87cea57
samimbey <samimbey@gmail.com>
2026-02-28 17:23:29 +0000

    File changes

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,6 +1,7 @@
 import { HelmetProvider } from "react-helmet-async";
 import Footer from "@/components/Footer";
 import ScrollToTop from "@/components/ScrollToTop";
+import LegacyRedirect from "@/components/LegacyRedirect";
 
 export default function Layout({ children, currentPageName }) {
   return (
